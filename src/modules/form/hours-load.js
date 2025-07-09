43e415b1b8aa00d6429604a74dd8ca8674830d4e
VictorAPuntel <victor.a.puntel@gmail.com>
2025-07-08 23:59:33 -0300

    Renderizando os horários

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours";
 
+const hours = document.getElementById("hours");
+
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
     //Recupera somente as horas
@@ -13,5 +15,14 @@ export function hoursLoad({ date }) {
       hour,
       available: isHourPast,
     };
+  });
+
+  //Renderizar os horários
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li");
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+    li.textContent = hour;
+    hours.append(li);
   });
 }
