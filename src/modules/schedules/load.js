09e3dd6dd5b16937dab1e48de69aa15fe6e6a5a0
VictorAPuntel <victor.a.puntel@gmail.com>
2025-07-10 00:25:31 -0300

    Criado métodos de busca dos agendamentos

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,16 @@
-import { hoursLoad } from "../form/hours-load";
+import { hoursLoad } from "../form/hours-load.js";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
+
 //Seleciona o input de data
 const selectedDate = document.getElementById("date");
-export function schedulesDay() {
+export async function schedulesDay() {
   //Obtem a data do imput
   const date = selectedDate.value;
+
+  //Busca na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+  console.log(dailySchedules);
+
   //Renderiza as horas disponíveis
   hoursLoad({ date });
 }
